13eb5f01192ac728b8e33f3be55ec4038ebb7d35
Osca-M <oscamwongera@gmail.com>
2020-12-05 15:09:11 +0300

    working with packages

---
+++ packages/packages_main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Introduction to packages in GoLang")
+}
+
+// run `go install` in the directory with package main to install the package in your $GOPATH
